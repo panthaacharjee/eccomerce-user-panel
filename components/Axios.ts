c47a64cdc2b019efc8fa159e81f32f72113eb2d6
Your Name <freelancerpantha@gmail.com>
2026-04-09 22:53:51 +0600

    first commit

--- components/Axios.ts
+++ components/Axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 
-// const staticUrl = `https://eccomerce-server.onrender.com`;
-const staticUrl = `http://localhost:4000`;
+const staticUrl = `https://eccomerce-server.onrender.com`;
+// const staticUrl = `http://localhost:4000`;
 const Axios = axios.create({
   baseURL: `${staticUrl}/api/v1`,
 });
