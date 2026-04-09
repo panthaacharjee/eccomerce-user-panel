ad016827dbf46d1cab0cd84f80e55213b8eea77d
Your Name <freelancerpantha@gmail.com>
2026-04-09 19:53:37 +0600

    first commit

--- components/Axios.ts
+++ components/Axios.ts
@@ -1,34 +1,10 @@
-// components/Axios.js
 import axios from "axios";
 
+
+const staticUrl = `https://eccomerce-server.onrender.com`;
+// const staticUrl = `http://localhost:4000`;
 const Axios = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: `${staticUrl}/api/v1`,
 });
-
-// Request interceptor
-Axios.interceptors.request.use(
-  (config) => {
-    // You can add auth token here if needed
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-// Response interceptor
-Axios.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    console.error("Axios error:", error.response?.data || error.message);
-    return Promise.reject(error);
-  }
-);
 
 export default Axios;
